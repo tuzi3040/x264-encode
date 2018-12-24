5447f2244d8a5259be02e0fbf01370c049ae2513
tuzi3040 <tuzi3040@aliyun.com>
2018-12-24 20:09:29 +0800

    update

--- statusupdate.py
+++ statusupdate.py
@@ -2,7 +2,7 @@ import logging
 import json
 import re
 import time
-import pymssql
+import pymysql
 import os
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkeci.request.v20180808.DescribeContainerLogRequest import DescribeContainerLogRequest
@@ -24,13 +24,14 @@ def handler(event, context):
 	database = os.environ.get('database')
 	username = os.environ.get('username')
 	password = os.environ.get('password')
-	conn = pymssql.connect(server=server, user=username, password=password, database=database)
+	conn = pymysql.connect(host=server, user=username, password=password, db=database, charset='utf8mb4')
 	cursor = conn.cursor()
 	
 	status = 'Container Group Created'
-	progress = 0.0
-	cursor.execute("INSERT INTO [dbo].[task] ( [Id], [VideoFilename], [Status], [Progress] ) VALUES ( "+ taskid + ", " + file + ", " + status+ ", " + progress + " )") 
+	progress = '0.0'
+	cursor.execute("UPDATE `task` SET `Status` = \"" + status + "\", `Progress` = " + progress + " WHERE `Id` = \"" + taskid + "\"") 
 	cursor.fetchone()
+	conn.commit()
 	
 	request = DescribeContainerLogRequest()
 	request.set_ContainerGroupId(cgid)
@@ -44,24 +45,32 @@ def handler(event, context):
 		
 		if lenrescontent <= 10:
 			status = 'Container Created'
-			cursor.execute("UPDATE [dbo].[task] SET [Status] = " + status + " WHERE [Id] = " + taskid ) 
+			cursor.execute("UPDATE `task` SET `Status` = \"" + status + "\" WHERE `Id` = \"" + taskid + "\"") 
+			cursor.fetchone()
+			conn.commit()
 		elif lenrescontent <= 300:
 			status = 'Initializing'
-			cursor.execute("UPDATE [dbo].[task] SET [Status] = " + status + " WHERE [Id] = " + taskid ) 
+			cursor.execute("UPDATE `task` SET `Status` = \"" + status + "\" WHERE `Id` = \"" + taskid + "\"") 
+			cursor.fetchone()
+			conn.commit()
 		else:
 			status = 'Running'
-			prop = re.compile(r'^\[(?:[1-9]?[0-9]{1}|100)\.[0-9]{1}%\]')
+			prop = re.compile(r'\[(?:[1-9]?[0-9]{1}|100)\.[0-9]{1}%\]')
 			prod = prop.findall(rescontent)
 			if prod:
-				progress = float(prod[-1][1:-2])
+				progress = prod[-1][1:-2]
 			else:
-				progress = 0.0
-			cursor.execute("UPDATE [dbo].[task] SET [Status] = " + status + ", [Progress] = " + progress + " WHERE [Id] = " + taskid ) 
-			endind = re.search(rescontent, r'^encoded \d+ frames')
+				progress = '0.0'
+			cursor.execute("UPDATE `task` SET `Status` = \"" + status + "\", `Progress` = " + progress + " WHERE `Id` = \"" + taskid + "\"") 
+			cursor.fetchone()
+			conn.commit()
+			endind = re.search(rescontent, r'encoded \d+ frames')
 			if endind:
 				status = 'Complete'
-				cursor.execute("UPDATE [dbo].[task] SET [Status] = " + status + " WHERE [Id] = " + taskid ) 
+				cursor.execute("UPDATE `task` SET `Status` = \"" + status + "\" WHERE `Id` = \"" + taskid + "\"") 
 				cursor.fetchone()
+				conn.commit()
 				break
-		cursor.fetchone()
 		time.sleep(3)
+	cursor.close()
+	conn.close()
